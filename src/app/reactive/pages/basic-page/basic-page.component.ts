a1ca1fd356e5468afcbd1357f67094c2c818e28b
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-10 08:55:04 -0300

    section 233 finished

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
@@ -15,9 +15,9 @@ export class BasicPageComponent {
   // })
 
   public myForm: FormGroup = this.fb.group({
-    name: [''],
-    price: [0],
-    inStorage: [0],
+    name: ['', [ Validators.required, Validators.minLength(3) ] ],
+    price: [0, [ Validators.required, Validators.min(0) ] ],
+    inStorage: [0, [ Validators.required, Validators.min(0) ] ],
   })
 
   constructor( private fb: FormBuilder ) {}
