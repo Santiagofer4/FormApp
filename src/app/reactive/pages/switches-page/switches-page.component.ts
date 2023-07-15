1259146f11521daa76ac80039077fc96235a8ec7
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-14 23:03:56 -0300

    section 246 finished

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,15 +6,24 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styles: [
   ]
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
 
   public myForm: FormGroup = this.fb.group({
     gender:             [ 'M', Validators.required ],
     wantNotifications:  [ true, Validators.required ],
     termsAndConditions: [ false, Validators.requiredTrue ]
   })
+
+  public person = {
+    gender: 'F',
+    wantNotifications: false
+  }
   
   constructor( private fb: FormBuilder ) {}
+
+  ngOnInit(): void {
+    this.myForm.reset( this.person )
+  }
 
   isValidField( field: string ): boolean | null {
     return this.myForm.controls[field].errors && this.myForm.controls[field].touched
@@ -26,6 +35,9 @@ export class SwitchesPageComponent {
       this.myForm.markAllAsTouched()
       return
     } 
+
+    const { termsAndConditions, ...newPerson } = this.myForm.value
+    this.person = newPerson
   }
 
 }
