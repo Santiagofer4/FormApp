d3d1161399134ea3a4baa9ddb7ac9d91e460acc6
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-16 00:01:58 -0300

    section 248 finished

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: [
-  ]
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [Validators.required, cantBeStrider ]],
+    password: ['', [ Validators.required,Validators.minLength(6) ]],
+    password2: ['', [Validators.required ]],
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  isValidField( field:string ) {
+
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched()
+  }
+  
 
 }
