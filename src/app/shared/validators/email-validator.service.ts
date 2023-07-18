b5aa609415d121a4da5632779a244161dfd1bd9d
santiagofer4 <santiagofernandezdelasierra@gmail.com>
2023-07-17 23:29:38 -0300

    section 252 finished

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -10,13 +10,34 @@ export class EmailValidatorService implements AsyncValidator {
   validate(control: AbstractControl): Observable<ValidationErrors | null> {
 
     const email = control.value
-    console.log({email})
 
-    return of({
-      emailTaken: true
+    const httpCallObservable = new Observable<ValidationErrors | null>( (subscriber) => {
+
+      if ( email === 'fernando@gmail.com' ) {
+        subscriber.next({ emailTaken: true })
+        subscriber.complete()
+      }
+
+      subscriber.next( null )
+      subscriber.complete()
     }).pipe(
-      delay(2000)
+      delay(3000)
     )
+
+    return httpCallObservable
+
   }
+
+  // validate(control: AbstractControl): Observable<ValidationErrors | null> {
+
+  //   const email = control.value
+  //   console.log({email})
+
+  //   return of({
+  //     emailTaken: true
+  //   }).pipe(
+  //     delay(2000)
+  //   )
+  // }
   
 }
